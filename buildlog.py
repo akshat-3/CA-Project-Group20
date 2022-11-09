e46894aab40e2bd135aba0b32dca7ef26bb9a1d7
akshat-3 <82972138+akshat-3@users.noreply.github.com>
2022-11-10 03:27:11 +0530

    Final Midterm

--- buildlog.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(filename="Logfile.log",
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-logger.info("Just an information")
